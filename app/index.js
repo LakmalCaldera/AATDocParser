badc1dd90583ce09ae33b266ffb0ecf1e327938f
Lakmal Caldera <lakmal.developer@gmail.com>
2017-12-09 15:24:30 +0530

    initial commit

---
+++ app/index.js
@@ -0,0 +1,231 @@
+var pdfreader = require('pdfreader');
+var model = require('./model.js');
+
+const styles = {
+    headerDark: {
+        fill: {
+            fgColor: {
+                rgb: 'FFFFFFFF'
+            }
+        },
+        font: {
+            color: {
+                rgb: '00000000'
+            },
+            sz: 12,
+            bold: true,
+            underline: true
+        }
+    },
+    cellPink: {
+        fill: {
+            fgColor: {
+                rgb: 'FFFFCCFF'
+            }
+        }
+    },
+    cellGreen: {
+        fill: {
+            fgColor: {
+                rgb: 'FF00FF00'
+            }
+        }
+    }
+};
+
+const CONST_HEADER = "Directory of Members  2017";
+
+const SUB_HEADER_TYPE_1 = "Governing Council  2016-2017"
+const SUB_HEADER_TYPE_2 = "Past Presidents"
+
+const excel_spec = {
+    name_category: {
+        displayName: 'Name/Category/Position',
+        headerStyle: styles.headerDark,
+        cellStyle: function (value, row) {
+            return value ? value : "-"
+        },
+        width: 120
+    },
+    office_addr: {
+        displayName: 'Office Address',
+        headerStyle: styles.headerDark,
+        cellFormat: function (value, row) {
+            return value ? value : "-"
+        },
+        width: '120'
+    },
+    office_tel: {
+        displayName: 'Office Tel No',
+        headerStyle: styles.headerDark,
+        cellStyle: function (value, row) {
+            return value ? value : "-"
+        },
+        width: 120
+    },
+    office_fax: {
+        displayName: 'Office Fax No',
+        headerStyle: styles.headerDark,
+        cellFormat: function (value, row) {
+            return value ? value : "-"
+        },
+        width: '120'
+    },
+    office_email: {
+        displayName: 'Office Email',
+        headerStyle: styles.headerDark,
+        cellStyle: function (value, row) {
+            return value ? value : "-"
+        },
+        width: 120
+    },
+    residence_addr: {
+        displayName: 'Residence Address',
+        headerStyle: styles.headerDark,
+        cellFormat: function (value, row) {
+            return value ? value : "-"
+        },
+        width: '120'
+    },
+    residence_tel: {
+        displayName: 'Residence Tel No',
+        headerStyle: styles.headerDark,
+        cellStyle: function (value, row) {
+            return value ? value : "-"
+        },
+        width: 120
+    },
+    residence_email: {
+        displayName: 'Residence Email',
+        headerStyle: styles.headerDark,
+        cellStyle: function (value, row) {
+            return value ? value : "-"
+        },
+        width: 120
+    }
+}
+
+const FILTER_TYPE_ACA = "ACA"
+const FILTER_TYPE_ACA_SPEC = {
+    name: 'ACA',
+    specification: excel_spec,
+    data: []
+}
+
+const FILTER_TYPE_FCA = "FCA"
+const FILTER_TYPE_FCA_SPEC = {
+    name: 'FCA',
+    specification: excel_spec,
+    data: []
+}
+
+const FILTER_TYPE_MBA = "MBA"
+const FILTER_TYPE_MBA_SPEC = {
+    name: 'MBA',
+    specification: excel_spec,
+    data: []
+}
+
+const FILTER_TYPE_FMAAT = "FMAAT"
+const FILTER_TYPE_FMAAT_SPEC = {
+    name: 'FMAAT',
+    specification: excel_spec,
+    data: []
+}
+
+const FILTER_TYPE_SAT = "SAT"
+const FILTER_TYPE_SAT_SPEC = {
+    name: 'SAT',
+    specification: excel_spec,
+    data: []
+}
+
+const FILTER_TYPE_MAAT = "MAAT"
+const FILTER_TYPE_MAAT_SPEC = {
+    name: 'MAAT',
+    specification: excel_spec,
+    data: []
+}
+
+
+var rows = {};
+var strRow = ""
+
+function printRows() {
+
+    FILTER_TYPE_ACA_SPEC.data.push({
+        name_category: "",
+        office_addr: "",
+        office_tel: "",
+        office_fax: "",
+        office_email: "",
+        residence_addr: "",
+        residence_tel: ""
+    })
+
+    //var profiles = {};
+    // console.log(rows)
+
+    Object.keys(rows)
+        .sort((y1, y2) => parseFloat(y1) - parseFloat(y2))
+        .forEach((y, index) => {
+            const row = (rows[y] || []).join('').trim()
+
+            // console.log(index, row, CONST_HEADER)
+            if (index == 0 && row != CONST_HEADER) {
+                return
+            }
+
+            strRow += row
+            if (index == (Object.keys(rows).length - 1)) {
+                //profiles[](strRow)
+                console.log(strRow)
+                var rowModel = new model(strRow)
+
+                if (rowModel.isACA()){
+                    FILTER_TYPE_ACA_SPEC.data.push(rowModel.getData())
+                }
+
+                if (rowModel.isFCA()){
+                    FILTER_TYPE_FCA_SPEC.data.push(rowModel.getData())
+                }
+
+                if (rowModel.isFMAAT()){
+                    FILTER_TYPE_FMAAT_SPEC.data.push(rowModel.getData())
+                }
+
+                if (rowModel.isSAT()){
+                    FILTER_TYPE_SAT_SPEC.data.push(rowModel.getData())
+                }
+
+                if (rowModel.isMAAT()){
+                    FILTER_TYPE_MAAT_SPEC.data.push(rowModel.getData())
+                }
+
+                if (rowModel.isMBA()){
+                    FILTER_TYPE_MBA_SPEC.data.push(rowModel.getData())
+                }   
+            }
+        });
+
+
+}
+
+new pdfreader.PdfReader().parseFileItems('./app/pdfs/page.pdf', function (err, item) {
+
+    console.log(item)
+
+    if (!item || item.page) {
+
+         // end of file, or page
+    printRows();
+    console.log('PAGE:', item.page);
+    rows = {}; // clear rows for next page
+    strRow = ""
+
+    }
+    else if (item.text) {
+        // accumulate text items into rows object, per line
+        (rows[item.y] = rows[item.y] || []).push(item.text);
+    }
+});
---
+++ app/model.js
@@ -0,0 +1,170 @@
+
+module.exports = function(str) {
+
+var data = {}
+
+// OFFICE SECTION
+var re = /(.*OFFICE\s?)(.*)(\s?RESIDENCE.*)/;
+var office_str = str.slice(0).replace(re, "$2");
+console.log("OFFICE SECTION : " + office_str);
+
+// RESIDENCE SECTION
+var re = /(.*\s?RESIDENCE\s?)(.*)/;
+var residence_str = str.slice(0).replace(re, "$2");
+console.log("RESIDENCE SECTION : " + residence_str);
+
+// Name
+var re = /((.*Governing Council  2016-2017\s?)|(.*Directory of Members  2017\s?))(.*)(\s?OFFICE.*)/;
+var newtext = str.slice(0).replace(re, "$4");
+console.log("Name/Category : " + newtext);
+data["name_category"] = newtext ? newtext : "-"
+
+// Categories
+var isACA = str.indexOf("ACA") != -1 ? true : false
+var isFCA = str.indexOf("FCA") != -1 ? true : false
+var isFMAAT = str.match(/[F]?(MAAT)/g).indexOf("FMAAT") != -1 ? true : false;
+var isSAT = str.indexOf("SAT") != -1 ? true : false
+var isMBA = str.indexOf("MBA") != -1 ? true : false
+var isMAAT = str.match(/[F]?(MAAT)/g).indexOf("MAAT") != -1 ? true : false;
+console.log("Categories: ")
+console.log(`ACA : ${isACA}`)
+console.log(`FCA : ${isFCA}`)
+console.log(`FMAAT : ${isFMAAT}`)
+console.log(`SAT : ${isSAT}`)
+console.log(`MAAT : ${isMAAT}`)
+
+// OFFICE flags
+var isOfficeContent = office_str != "" ? true : false
+var isOfficeTel = office_str.indexOf("Tel No:") != -1 ? true : false
+var isOfficeFax = office_str.indexOf("Fax:") != -1 ? true : false
+var isOfficeEmail = office_str.indexOf("E-Mail:") != -1 ? true : false
+
+console.log(`isOfficeContent : ${isOfficeContent}`)
+console.log(`isOfficeTel : ${isOfficeTel}`)
+console.log(`isOfficeFax : ${isOfficeFax}`)
+console.log(`isOfficeEmail : ${isOfficeEmail}`)
+
+if (isOfficeContent) {
+
+// OFFICE addr
+var re = /(.*OFFICE\s?)(.*?)((\s?Tel No:.*)|(\s?Fax:.*)|(\s?E-Mail:.*)|(\s?RESIDENCE.*))/;
+var newtext = str.slice(0).replace(re, "$2");
+console.log("Office address : " + newtext);
+data["office_addr"] = newtext ? newtext : "-"
+
+
+// OFFICE tel
+if (isOfficeTel) {
+var re = /(.*Tel No:.\s?)(.*)((\s?Fax:.*)|(\s?E-Mail:.*))/;
+var newtext = office_str.slice(0).replace(re, "$2");
+    console.log("Office Tel : " + newtext);
+    data["office_tel"] = newtext ? newtext : "-"
+} else {
+    data["office_tel"] = "-"
+}
+
+
+// OFFICE fax
+if (isOfficeFax) {
+var re = /(.*Fax:.\s?)(.*?)(\s?E-Mail:.*)/;
+var newtext = office_str.slice(0).replace(re, "$2");
+console.log("Office Fax : " + newtext);
+data["office_fax"] = newtext ? newtext : "-"
+} else {
+    data["office_fax"] = "-"
+}
+
+
+// OFFICE Email
+if (isOfficeEmail) {
+var re = /(.*E-Mail:.\s?)(.*?)/;
+var newtext = office_str.slice(0).replace(re, "$2");
+console.log("Office Email : " + newtext);
+data["office_email"] = newtext ? newtext : "-"
+} else {
+    data["office_email"] = "-"
+}
+
+} else {
+
+    data["office_addr"] = "-"
+    data["office_fax"] = "-"
+    data["office_tel"] = "-"
+    data["office_email"] = "-"
+}
+
+// ===================================================
+
+// Residence flags
+var isResidenceContent = residence_str != "" ? true : false
+var isResidenceTel = residence_str.indexOf("Tel No:") != -1 ? true : false
+var isResidenceFax = residence_str.indexOf("Fax:") != -1 ? true : false
+var isResidenceEmail = residence_str.indexOf("E-Mail:") != -1 ? true : false
+
+console.log(`isResidenceContent : ${isResidenceContent}`)
+console.log(`isResidenceTel : ${isResidenceTel}`)
+console.log(`isResidenceFax : ${isResidenceFax}`)
+console.log(`isResidenceEmail : ${isResidenceEmail}`)
+
+// Residence addr
+var re = /(.*RESIDENCE\s?)(.*?)((\s?Tel No:.*)|(\s?Fax:.*)|(\s?E-Mail:.*))?/;
+var newtext = str.slice(0).replace(re, "$2");
+console.log("Residence address : " + newtext);
+data["residence_addr"] = newtext ? newtext : "-"
+
+
+if (isResidenceContent){
+
+// Residence tel
+if (isResidenceTel) {
+var re = /(.*Tel No:.\s?)(.*)((\s?Fax:.*)|(\s?E-Mail:.*))?/;
+var newtext = residence_str.slice(0).replace(re, "$2");
+console.log("Residence Tel : " + newtext);
+    data["residence_tel"] = newtext ? newtext : "-"
+} else {
+    data["residence_tel"] = "-"
+}
+
+// Residence Email
+if (isResidenceEmail) {
+var re = /(.*E-Mail:.\s?)(.*?)/;
+var newtext = residence_str.slice(0).replace(re, "$2");
+console.log("Residence Email : " + newtext);
+    data["residence_email"] = newtext ? newtext : "-"
+} else {
+    data["residence_email"] = "-"
+}
+
+} else {   
+    data["residence_addr"] = "-"
+    data["residence_tel"] = "-"
+    data["residence_email"] = "-"
+}
+
+
+console.log(data)
+
+return {
+    isACA: () => {
+        return isACA
+    }, 
+    isFCA: () => {
+        return isFCA
+    }, 
+    isFMAAT: () => {
+        return isFMAAT
+    },
+    isMAAT: () => {
+        return isMAAT
+    },
+    isSAT: () => {
+        return isSAT
+    },
+    isMBA: () => {
+        return isMBA
+    },
+    getData:() => {
+        return data
+    }
+}
+}
